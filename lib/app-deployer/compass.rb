fc1282cf4f3cbd39ebe29eba460afc41df3904b5
Ricky Dunlop <hello@rickydunlop.co.uk>
2013-02-06 16:53:26 +0000

    added compass file

--- lib/app-deployer/compass.rb
+++ lib/app-deployer/compass.rb
@@ -2,7 +2,7 @@ Capistrano::Configuration.instance(:must_exist).load do
 	namespace :compass do
 		desc 'Compiles Compass files into stylsheets'
 		task :compile do
-			run("cd #{latest_release}; bundle exec compass compile --output-style nested --force -e production")
+			run("cd #{latest_release}/webroot; compass compile --output-style nested --force -e production")
 		end
 	end
 end
