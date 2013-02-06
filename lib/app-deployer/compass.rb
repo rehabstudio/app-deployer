70d3024245547843f84962603868dde6a03bd4be
Ricky Dunlop <hello@rickydunlop.co.uk>
2013-02-06 12:15:44 +0000

    fixed typo

--- lib/app-deployer/compass.rb
+++ lib/app-deployer/compass.rb
@@ -1,7 +1,7 @@
 Capistrano::Configuration.instance(:must_exist).load do
 	namespace :compass do
 		desc 'Compiles Compass files into stylsheets'
-		task compile do
+		task :compile do
 			run("cd #{latest_release}; bundle exec compass compile --output-style nested --force -e production")
 		end
 	end
